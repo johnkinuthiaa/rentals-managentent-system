0556b9c93ffb4189af048617a1711fa5f47ca481
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-01 18:02:47 +0300

    user login and registration

--- src/main/java/com/slippery/rentalmanagementsystem/mail/Register.java
+++ src/main/java/com/slippery/rentalmanagementsystem/mail/Register.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+
 @Service
 public class Register {
     private final JavaMailSender javaMailSender;
